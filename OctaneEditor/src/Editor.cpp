4cccab6a84834d6f3ed17aebb3cb9c3af23b620d
octane-xr <octirie@gmail.com>
2025-07-31 16:21:43 -0300

    Added layer for attachment test

--- OctaneEditor/src/Editor.cpp
+++ OctaneEditor/src/Editor.cpp
@@ -1,7 +1,24 @@
 #include <Octane.h>
+using namespace Octane;
+
+struct MyLayer : AppInterface
+{
+    OCTANE_INLINE void OnUpdate()
+    {
+        OCTANE_INFO("OnUpdate()");
+    }
+
+    OCTANE_INLINE void OnStart()
+    {
+        OCTANE_TRACE("OnStart()");
+    }
+};
 
 int32_t main(int32_t argc, char** argv)
 {
-    OCTANE_INFO("Editor Started!");
+    auto app = new Application();
+    app->AttachmentLayer<MyLayer>();
+    app->RunContext();
+    
     return 0;
 }
